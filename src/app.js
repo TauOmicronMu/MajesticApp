c74eae9a2d25f069c3b4a7ec85a010b04615c155
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-01-28 21:02:03 +0000

    Finished work on the application.

--- src/app.js
+++ src/app.js
@@ -7,40 +7,42 @@ $(document).ready(function () {
      */  	
     var rankdiff = function(rank, oldRank) {
 	if(rank < oldRank) {
-	    return "<b>(↑) </b>"
+	    return "<b>(↑) </b>";
 	}
 	else if(rank == oldRank) {
-	    return "<b>(-) </b>"
+	    return "<b>(-) </b>";
 	}
 	else {
-	    return "<b>(↓) </b>"
-	}
+	    return "<b>(↓) </b>";
+        };
+    };
     /*
      * Creates a new CanvasJS Chart called "piechart" and 
      * styles it, then adds the data and finally renders 
      * it to the page.
      */
     var chart = new CanvasJS.Chart("piechart", {
-        title: {text: "Website Quantity by TLD"}, 
+        title: {text: "Website Quantity by TLD" }, 
         animationEnabled: true, 
+        backgroundColor: "#add8e6",
         legend: {
             verticalAlign : "center",
             horizontalAlign : "left",
-            fontSize : 15,
-        } 
+            fontSize : 18
+        }, 
         theme : "theme2",
         data : [ {
-            type : "pie"
-            indexLabelFontFamily: "Garamond",       
+            type : "pie",
+            indexLabelFontFamily: "Quicksand",       
 	    indexLabelFontSize: 20,
 	    indexLabel: "{label} {y}%",
 	    startAngle:-20,      
 	    showInLegend: true,
 	    toolTipContent:"{legendText} {y}%",
-	    dataPoints: //TODO : Grab the data in JSON format.
-        }]
+	    dataPoints: datapoints
+        } ]
     }); chart.render(); 
-
+});
     /*
      * Legacy code that was used to generate a simple HTML table.
      *
